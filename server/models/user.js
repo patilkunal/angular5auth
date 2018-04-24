e7088404144fdd76d0a2d6555eae32b59dca60e4
Kunal Patil <kunalpatil@yahoo.com>
2018-04-23 23:02:21 -0400

    Setup to connect to Local Mongo DB

---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+    email:String,
+    password:String,
+    name: String
+})
+
+//defines 'user' schema with model (schema) defined above and would connect to 'users' collection on MongoDB
+module.exports = mongoose.model('user', userSchema, 'users');
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose');
+
+//DB connection string
+const dbconn = "mongodb://127.0.0.1:27017/eventsdb"
+
+mongoose.connect(dbconn, err => {
+    if(err) {
+        console.error('Error!' + err);
+    } else {
+        console.log('Connected to mongodb');
+    }
+})
+
 router.get('/', (req, res) =>{
     res.send('From API route');
 })
