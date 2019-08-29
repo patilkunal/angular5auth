bc64f759a6944c01b1310d46540eb74fc0b1547a
Kunal Patil <kunal.patil@cox.com>
2019-08-29 13:59:32 -0400

    last minute changes

--- ngApp/src/app/special-events/special-events.component.ts
+++ ngApp/src/app/special-events/special-events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {EventService} from '../event.service';
 
 @Component({
   selector: 'app-special-events',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialEventsComponent implements OnInit {
 
-  constructor() { }
+  events = [];
+  constructor(private eventService: EventService) { }
 
   ngOnInit() {
+    this.eventService.getSpecialEvents()
+      .subscribe(
+        res => this.events = res,
+        err => console.log(err)
+      ) 
   }
 
 }
